1e53c92a012c6bb81a152617987f282d05dab783
nobelium24 <ogunbaja07@gmail.com>
2024-02-08 16:45:26 +0100

    made minor change in start.sh and .env

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 source .env
 
 # Get the ID of the running PostgreSQL container
-CONTAINER_ID=$(docker ps -q --filter name=wsAndSQL)
+CONTAINER_ID=$(docker ps -q --filter name=wsandsql)
 
 # Connect to PostgreSQL and create the database if it doesn't exist
 docker exec -it $CONTAINER_ID bash -c "PGPASSWORD=$DB_PASSWORD psql -h localhost -U $DB_USERNAME -tc \"SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'\" | grep -q 1 || PGPASSWORD=$DB_PASSWORD psql -h localhost -U $DB_USERNAME -c \"CREATE DATABASE $DB_NAME\""
